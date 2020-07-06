20412a03722c136f125d82ae8bc682ea36501c5f
londonFlynn <47341087+londonFlynn@users.noreply.github.com>
2020-07-06 10:10:07 -0600

    added base models and caclulations

---
+++ WebDPRCalc/Models/AttackDPRFragment.cs
@@ -0,0 +1,10 @@
+﻿namespace WebDPRCalc.Models
+{
+    public class AttackDPRFragment
+    {
+        public int ac { get; set; }
+        public double hitChance { get; set; }
+        public double critChance { get; set; }
+        public double averageDamage { get; set; }
+    }
+}
