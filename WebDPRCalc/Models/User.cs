f3f4144ab13d0e91d211e8ed41d6cb09d27ad1e8
MishaFinley <47871308+MishaFinley@users.noreply.github.com>
2020-07-07 09:10:21 -0600

    Added User class

--- WebDPRCalc/Models/User.cs
+++ WebDPRCalc/Models/User.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 
 namespace WebDPRCalc.Models
 {
@@ -9,18 +11,36 @@ namespace WebDPRCalc.Models
     {
         private string username;
         private byte[] password;
-        //private Attack[] Attacks;
+        private Attack[] Attacks;
 
 
 
         public bool validPassword(string password)
         {
+            byte[] hashedPassword = hashPassword(password);
+            if(hashedPassword == this.password)
+            {
+                return true;
+            }
             return false;
         }
 
         public byte[] hashPassword(string password)
         {
-            return null;
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+            byte[] hashed = KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8);
+
+
+            return hashed;
         }
     }
 }
