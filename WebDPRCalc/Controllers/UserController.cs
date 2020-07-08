b4c2887c376958e9359c0126078ab142e2380ea6
ElijahHarris <47341441+ElijahHarris@users.noreply.github.com>
2020-07-08 09:05:29 -0600

    Add mock login

--- WebDPRCalc/Controllers/UserController.cs
+++ WebDPRCalc/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
 namespace WebDPRCalc.Controllers
 {
@@ -12,10 +13,27 @@ namespace WebDPRCalc.Controllers
         {
             return View();
         }
-
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if (username != null && password != null)
+            {
+                HttpContext.Session.SetString("username", username);
+                return RedirectToAction("Index", "Home");
+            }
+                return View();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+
+            return RedirectToAction("Index","Home");
         }
 
         public IActionResult CreateAccount()
