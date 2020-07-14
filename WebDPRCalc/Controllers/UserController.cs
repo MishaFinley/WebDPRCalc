669a3a5c90b3cb570e0f160b8bb1be78a0e54199
londonFlynn <47341087+londonFlynn@users.noreply.github.com>
2020-07-13 21:44:39 -0600

    Change Password

--- WebDPRCalc/Controllers/UserController.cs
+++ WebDPRCalc/Controllers/UserController.cs
@@ -68,6 +68,19 @@ namespace WebDPRCalc.Controllers
                 return View();
             }
         }
+        [HttpPost]
+        public IActionResult Profile(string password, string passwordcon)
+        {
+            string username = HttpContext.Session.GetString("username");
+            User check = UserDatabaseInterface.readUser(username);
+            if (!(check is null) && password.Equals(passwordcon))
+            {
+                check.password = Models.User.hashPassword(password, username);
+                UserDatabaseInterface.updateUser(check);
+            }
+            ViewBag.profile = check;
+            return View();
+        }
 
     }
 }
